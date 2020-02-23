56587043f11853cc4a9bb598c47ba20737aaf769
James Wallis <james.wallis1@ibm.com>
2020-02-23 20:32:41 +0000

    Add initial header and icon bar components
    Signed-off-by: James Wallis <james.wallis1@ibm.com>

---
+++ website/components/IconBar.js
@@ -0,0 +1,39 @@
+import styles from './IconBar.module.css';
+
+const icons = [
+    {
+        src: "/icons/Icons-Parking.png",
+        alt: "parking icon",
+    },
+    {
+        src: "/icons/Icons-Call.png",
+        alt: "call icon",
+    },
+    {
+        src: "/icons/Icons-email.png",
+        alt: "email icon",
+    },
+    {
+        src: "/icons/Icons-Insta.png",
+        alt: "instagram icon",
+    },
+    {
+        src: "/icons/Icons-Facebook.png",
+        alt: "facebook icon",
+    },
+    {
+        src: "/icons/Icons-Twitter.png",
+        alt: "twitter icon",
+    },
+]
+
+export default () => {
+    return <div className={styles.container}>
+        {icons.map(icon => {
+            const { src, alt } = icon;
+            return <div className={styles.icon}>
+                <img src={src} alt={alt} />
+            </div>
+        })}
+    </div>
+}
