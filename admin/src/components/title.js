f7c8e7e26050b0059591e4468a040b0525c1497e
James Wallis <j@wallis.dev>
2021-07-09 21:00:51 +0100

    Change admin logo
    Signed-off-by: James Wallis <j@wallis.dev>

--- admin/src/components/title.js
+++ admin/src/components/title.js
@@ -3,7 +3,8 @@ import React from 'react';
 function TitleBox() {
     return (
         <div style={{ textAlign: 'center', marginTop: 10, marginBottom: 10 }}>
-            <img style={{ width: '100%' }} src="/SalsaMishAdminLogo.png" alt="Salsa Mish Admin Logo" />
+            <img style={{ width: '90%' }} src="/SalsaMishLogo.png" alt="Salsa Mish Logo" />
+            <img style={{ width: '40%' }} src="/admin.png" alt="it says admin" />
         </div>
     );
 }
