29cbc034faf731e6cc50d07c3e2d8df32a781c9a
James Wallis <j@wallis.dev>
2021-07-13 23:16:10 +0100

    Add Google Analytics
    Signed-off-by: James Wallis <j@wallis.dev>

--- 2021-website/pages/_document.tsx
+++ 2021-website/pages/_document.tsx
@@ -1,11 +1,31 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
 
+const GA_MEASUREMENT_ID = 'G-4RD9YX6448'
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <script
+                async
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                    __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                });
+                `,
+                }}
+            />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
