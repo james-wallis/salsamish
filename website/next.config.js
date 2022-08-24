600a3128997a9f5d7d0b0bbf078d056c115ddafb
James Wallis <j@wallis.dev>
2022-08-24 22:43:12 +0100

    feat: add redirects to socials

--- website/next.config.js
+++ website/next.config.js
@@ -4,4 +4,28 @@ module.exports = {
   env: {
     GOOGLE_MAPS_APIKEY,
   },
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/james-wallis/salsamish',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/SalsaMish',
+        permanent: true,
+      },
+      {
+        source: '/clubhouse',
+        destination: 'https://www.clubhouse.com/club/salsa-mish',
+        permanent: true,
+      },
+      {
+        source: '/facebook',
+        destination: 'https://www.facebook.com/SalsaMish',
+        permanent: true,
+      },
+    ]
+  },
 }
